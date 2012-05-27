4f4ebd477c1d16ddb23c26ba75e67e2fa84ca060
Samnan ur Rehman <samnan.akhoond@gmail.com>
2012-05-27 21:28:29 +0500

    updated source files

--- source/javascripts/jquery.github_v2.js
+++ source/javascripts/jquery.github_v2.js
@@ -1,5 +1,5 @@
-jQuery.githubUser = function(usernames, callback) {
+$.githubUser = function(usernames, callback) {
   $.each(usernames, function(index, username) {
-    jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
+    $.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
   });
 }
